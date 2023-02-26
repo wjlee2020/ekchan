041ccbbf670873e4a3529e97d06e22ca3b22499b
wjlee2020 <w.lee@churadata.okinawa>
2023-02-26 12:46:31 +0900

    feat: keihi screen

--- app/(tabs)/keihi.tsx
+++ app/(tabs)/keihi.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import Keihi from '../../screens/Keihi';
 
 export default function KeihiScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Keihi</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/keihi.tsx" />
+      <Keihi />
     </View>
   );
 }
---
+++ screens/Keihi.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Text, View } from '../components/Themed';
+
+export default function Keihi() {
+  return (
+    <View>
+      <View style={styles.getStartedContainer}>
+        <Text
+          style={styles.getStartedText}
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          Open up the code for this screen:
+        </Text>
+
+        <Text
+          style={styles.getStartedText}
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          Change any of the text, save the file, and your app will automatically update.
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  getStartedText: {
+    fontSize: 17,
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+});
