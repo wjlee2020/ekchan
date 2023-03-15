1fd7a18e764edfd9357cbdfb9515e108b17938dd
wjlee2020 <122327356+wjlee2020-off@users.noreply.github.com>
2023-03-16 08:29:03 +0900

    refactor: remove dumb comment

--- app/tsuika.tsx
+++ app/tsuika.tsx
@@ -20,7 +20,6 @@ export default function ModalScreen() {
 
       <AddKehi />
 
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
